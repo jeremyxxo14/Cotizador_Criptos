80a05c82c78d36c25787386713c975852be17d6a
Jeremy Muñoz <jeremyxxo@gmail.com>
2023-08-01 21:33:04 -0400

    Proyecto Terminado

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,60 @@
+import styled from "@emotion/styled"
+
+const Contenedor = styled.div`
+
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+    @media (max-width: 992px){
+        justify-content: center;
+    }
+
+`
+
+const Imagen = styled.img`
+
+    display: block;
+    width: 120px;
+`
+
+const Texto = styled.p`
+
+    font-size: 16px;
+    span{
+        font-weight: 700;
+    }
+
+    
+`
+const Precio = styled.p`
+
+    font-size: 26px;
+    span{
+        font-weight: 700;
+    }
+
+`
+
+const Resultado = ({resultado}) => {
+    const {PRICE, HIGHDAY,LOWDAY, CHANGEPCT24HOUR,IMAGEURL,LASTUPDATE} = resultado
+    return (
+        <Contenedor>
+            <Imagen 
+                src={`https://cryptocompare.com/${IMAGEURL}`} 
+                alt="imagen cripto" 
+            />
+            <div>
+                <Precio>El precio es de: <span>{PRICE}</span></Precio>
+                <Texto>El precio mas alto del día: <span>{HIGHDAY}</span></Texto>
+                <Texto>El precio mas bajo del día: <span>{LOWDAY}</span></Texto>
+                <Texto>Variacion últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+                <Texto>Ultima Actualizacion del dia<span>{LASTUPDATE}</span></Texto>
+            </div>
+        </Contenedor>
+    )
+}
+
+export default Resultado
---
+++ src/data/monedas.js
@@ -0,0 +1,14 @@
+const monedas =[
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'MXN', nombre: 'Peso de Mexicano'},
+    {id: 'CLP', nombre: 'Peso Chileno'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'}
+]
+
+
+
+
+export{
+    monedas
+}
